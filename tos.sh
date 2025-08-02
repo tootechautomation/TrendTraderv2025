ac48cbd3024ad9b4a8e6a526d023e2cc094e630c
tootechautomation <50243547+tootechautomation@users.noreply.github.com>
2025-08-01 22:09:08 -0800

    Create tos.sh

---
+++ tos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+sudo rm -rf /tmp/ad_*
+sudo rm -rf /tmp/mdrcache*
+sudo rm -rf /tmp/+~JF*
+sudo rm -rf /tmp/Screenshot*
+sudo rm -rf /tmp/.org.chrom*
+
+while $true; do java -jar /home/$(whoami)/Desktop/TrendTrader2025/TrendTraderv2025.jar
+
+
+
