e3b288baa9588eceb9a1db0d3ef081fca3914165
tboone <tboone@bn-lt-01>
2025-08-03 21:53:04 -0800

    all

--- tos.sh
+++ tos.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
-sudo rm -rf /tmp/ad_*
-sudo rm -rf /tmp/mdrcache*
-sudo rm -rf /tmp/+~JF*
-sudo rm -rf /tmp/Screenshot*
-sudo rm -rf /tmp/.org.chrom*
+rm -rf /tmp/ad_*
+rm -rf /tmp/mdrcache*
+rm -rf /tmp/+~JF*
+rm -rf /tmp/Screenshot*
+rm -rf /tmp/.org.chrom*
 
 while $true; do java -jar /home/$(whoami)/Documents/TrendTrader2025/TrendTraderv2025.jar;done
 
