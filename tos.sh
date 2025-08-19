1ea49fef6fff6b375fe8c28673b8499751d9a696
tboone <tboone@bn-lt-01>
2025-08-19 11:30:20 -0800

    all

--- tos.sh
+++ tos.sh
@@ -8,9 +8,7 @@ rm -rf /tmp/.org.chrom*
 
 while $true; 
 do 
-    java -jar /home/$(whoami)/Documents/TrendTraderv2025/PLRunner/plrunner.jar &
     java -jar /home/$(whoami)/Documents/TrendTraderv2025/TrendTraderv2025.jar 
-    for i in $(ps -aux | grep plrunner | awk '{print $2}'); do kill $i;done
 done
 
 
