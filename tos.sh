34f87fb2e98cd89b1560de96725ceb8c1226cb34
tboone <tboone@bn-lt-01>
2025-08-26 14:37:22 -0800

    all

--- tos.sh
+++ tos.sh
@@ -6,7 +6,7 @@ rm -rf /tmp/+~JF*
 rm -rf /tmp/Screenshot*
 rm -rf /tmp/.org.chrom*
 
-TT_PATH="/home/$(whoami)/Documents/TrendTraderv2025/"
+TT_PATH="/home/$(whoami)/Documents/TrendTraderv2025"
 API_VERSION="2.0.5"
 if [ ! -f  "$TT_PATH/sikulixapi-$API_VERSION-lux.jar" ]; then
     LOCAL_PATH="$TT_PATH/sikulixapi-$API_VERSION-lux.jar"
@@ -17,7 +17,9 @@ fi
 
 while $true; 
 do 
-    java -jar /home/$(whoami)/Documents/TrendTraderv2025/TrendTraderv2025.jar 
+    export CLASSPATH="$CLASSPATH:$TT_PATH/sikulixapi-$API_VERSION-lux.jar"
+    java -cp "$TT_PATH/sikulixapi-$API_VERSION-lux.jar;$TT_PATH/TrendTraderv2025.jar" org.sikuli.script.Runner
+    #java -jar /home/$(whoami)/Documents/TrendTraderv2025/TrendTraderv2025.jar 
 done
 
 
