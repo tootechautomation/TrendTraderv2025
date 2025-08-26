4a13bd8903f3cb661012ed0f45d381ab92280abe
tboone <tboone@bn-lt-01>
2025-08-26 14:21:10 -0800

    all

--- tos.sh
+++ tos.sh
@@ -6,6 +6,13 @@ rm -rf /tmp/+~JF*
 rm -rf /tmp/Screenshot*
 rm -rf /tmp/.org.chrom*
 
+TT_PATH="/home/$(whoami)/Documents/TrendTraderv2025/"
+API_VERSION="2.0.5"
+if [ ! -f  "./sikulixapi-$(API_VERSION)-lux.jar" ]; then
+    echo "File not found!"
+    curl -o $TT_PATH/sikulixapi-$(API_VERSION)-lux.jar "https://launchpad.net/sikuli/sikulix/$(API_VERSION)/+download/sikulixapi-$(API_VERSION)-lux.jar"   
+fi
+
 while $true; 
 do 
     java -jar /home/$(whoami)/Documents/TrendTraderv2025/TrendTraderv2025.jar 
