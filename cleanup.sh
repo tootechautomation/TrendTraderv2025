f97fa421e48297ae9da71c75ac64db848cf8bd07
tboone <tboone@bn-lt-01>
2025-09-02 12:22:41 -0800

    all

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,8 @@
 
 while($True);
 do
+    sleep 5
+
     res=$(ps -aux | grep TrendTrader | grep 2025 | awk '{print $2}' | head -n 1)
     if [ -z $res ]; then 
         echo EXIT
@@ -9,6 +11,6 @@ do
     fi
 
     rm -rf /tmp/mdrcache*
-    find /tmp -type f -name \"*.png\" -mmin +1 -delete
-    sleep 5
+    find /tmp -type f -name "*.png" -mmin +1 ! -name "grayscale_processed_trendtrader.png" -delete
+
 done
