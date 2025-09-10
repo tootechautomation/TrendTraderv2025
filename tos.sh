d12e3732bb5362c16085635e8867353df771886c
tboone <tboone@bn-lt-01>
2025-09-09 21:52:42 -0800

    all

--- tos.sh
+++ tos.sh
@@ -6,6 +6,10 @@ rm -rf /tmp/+~JF*
 rm -rf /tmp/Screenshot*
 rm -rf /tmp/.org.chrom*
 
+
+CHECK_LOCK=$(gsettings get org.mate.screensaver lock-enabled)
+CHECK_IDLE=$(gsettings get org.mate.screensaver idle-activation-enabled)
+
 TT_PATH="/home/$(whoami)/Documents/TrendTraderv2025"
 API_VERSION="2.0.5"
 if [ ! -f  "$TT_PATH/sikulixapi-$API_VERSION-lux.jar" ]; then
@@ -32,14 +36,20 @@ fi
 
 bash ./cleanup.sh &
 
+
+if [ "$CHECK_LOCK" ]; then
+    echo "YES1"
+    gsettings set org.mate.screensaver lock-enabled false
+fi
+
+if [ "$CHECK_IDLE" ]; then
+    echo "YES2"
+    gsettings set org.mate.screensaver idle-activation-enabled false
+fi
+
 while $true; 
 do 
     export CLASSPATH=$CLASSPATH:$TT_PATH/sikulixapi-$API_VERSION-lux.jar
     java -jar $TT_PATH/sikulixapi-$API_VERSION-lux.jar -r $TT_PATH/TrendTraderv2025.jar #" org.sikuli.script.Runner
     #java -jar /home/$(whoami)/Documents/TrendTraderv2025/TrendTraderv2025.jar 
 done
-
-# VNC XFCE
-#XAUTHORITY=$HOME/.Xauthority
-#export XAUTHORITY   
-
